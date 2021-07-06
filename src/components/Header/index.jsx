795f062232346c26052134a7ecdf26764648dda3
Audrey Diez <audrey.diez@gmail.com>
2021-07-06 17:29:53 +0200

    feat: chapter 2

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <nav>
+      <Link to="/">Accueil</Link>
+      <Link to="/survey/1">Questionnaire</Link>
+      <Link to="/Results">Résultats</Link>
+      <Link to="/Freelances">Freelances</Link>
+    </nav>
+  )
+}
+
+export default Header
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import './index.css'
-import Home from './Pages/Home'
+import Home from './Pages/Home/Home'
+import Survey from './Pages/Survey/Survey'
+import Header from './components/Header'
 import reportWebVitals from './reportWebVitals'
+import Error from './components/Error'
+import Results from './Pages/Results/Results'
+import Freelances from './Pages/Freelances/Freelances'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/survey/:questionNumber">
+          <Survey />
+        </Route>
+        <Route path="/results">
+          <Results />
+        </Route>
+        <Route path="/freelances">
+          <Freelances />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
